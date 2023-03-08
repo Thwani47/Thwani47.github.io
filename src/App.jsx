c66cb5abbcb328ad62b54c928bb91f7156ac534d
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-03-08 21:48:37 +0200

    fixed spelling mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div className="App">Site Devlopment in progress....</div>;
+	return <div className="App">Site Development in progress....</div>;
 }
 
 export default App;
