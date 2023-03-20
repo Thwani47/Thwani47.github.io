f25bc611b9c459856f97358801c6ed354113df36
Thwani Sithole <Thwani.Sithole@derivco.co.za>
2023-03-21 00:01:28 +0200

    added FE mentor challenges

---
+++ src/ChallengeCard.jsx
@@ -0,0 +1,22 @@
+import { Card } from 'flowbite-react';
+import React from 'react';
+
+export default function ChallengeCard({challengeUrl, title, githubLink, liveUrl, imageSrc, description}) {
+	return (
+		<div className="max-w-sm hover:scale-105">
+			<Card
+                href={liveUrl}
+				imgAlt={title}
+                imgSrc={imageSrc}
+			>
+				<h5 className="text-3xl font-bold tracking-tight mt-2 mb-2 text-gray-900 dark:text-white">
+					{title}
+				</h5>
+				<p className="font-normal text-gray-700 dark:text-gray-400">
+					{description}
+				</p>
+                <a href={githubLink} target="_blank" className="mt-4 text-lg">Source code</a>
+			</Card>
+		</div>
+	);
+}
